e43612af4cd0635959f0a402892e0186eb1c0f0b
Babasola Oso <babasola.oso@cbitechnologiesltd.com>
2026-03-30 13:52:19 +0100

    to make prisma work on render

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
+require('dotenv').config()
 import express from "express"
 import uploadRoutes from "./routes/uploadRoutes"
 
+const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json())
+app.get("/", (_, res) => { res.send("Welcome To Our Data Pipeline !") })
 
 app.use("/api/data/ingestion", uploadRoutes)
 
-app.listen(3000, () => {
- console.log("Server running on port 3000")
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
